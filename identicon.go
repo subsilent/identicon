4776397e9ac863a6d0646ec0f54fff41ac1008ae
eliquious <max.franks@synapse-wireless.com>
2015-06-14 22:47:31 -0500

    Adding cache layer

--- identicon.go
+++ identicon.go
@@ -13,7 +13,8 @@ import (
 )
 
 var (
-	address = flag.String("addr", ":3000", "Address to bind to")
+	address   = flag.String("addr", ":3000", "Address to bind to")
+	cachesize = flag.Int("cachesize", 128, "Size of image cache in MB")
 )
 
 func main() {
@@ -25,7 +26,10 @@ func main() {
 	logger := log.NewLogger(writer, "identicon")
 
 	// Start web server
-	router := web.NewServer(logger)
+	router := web.NewServer(web.Options{
+		Logger:    logger,
+		CacheSize: *cachesize,
+	})
 
 	gracehttp.Serve(
 		&http.Server{Addr: *address, Handler: router},
--- web/server.go
+++ web/server.go
@@ -2,8 +2,10 @@ package web
 
 import (
 	"encoding/base64"
+	"runtime/debug"
 	"time"
 
+	"github.com/coocood/freecache"
 	identicon "github.com/dgryski/go-identicon"
 	"github.com/gin-gonic/gin"
 	log "github.com/mgutz/logxi/v1"
@@ -11,13 +13,26 @@ import (
 
 var Key = []byte{0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF}
 
-func NewServer(logger log.Logger) *gin.Engine {
+type Options struct {
+	Logger    log.Logger
+	CacheSize int
+}
+
+func NewServer(opts Options) *gin.Engine {
+
+	// options
+	logger := opts.Logger
+
+	cacheSize := opts.CacheSize * 1024 * 1024
+	logger.Info("Creating cache", "size", cacheSize)
+	cache := freecache.NewCache(cacheSize)
+	debug.SetGCPercent(20)
 
 	// create router
 	router := gin.New()
 	router.Use(gin.Recovery(), Logger(logger))
 	router.GET("/icon/:name", BasicImageRoute(logger))
-	router.GET("/encoded/:name", EncodedImageRoute(logger))
+	router.GET("/encoded/:name", EncodedImageRoute(logger, cache))
 	return router
 }
 
@@ -58,17 +73,31 @@ func BasicImageRoute(l log.Logger) gin.HandlerFunc {
 	}
 }
 
-func EncodedImageRoute(l log.Logger) gin.HandlerFunc {
+func EncodedImageRoute(l log.Logger, cache *freecache.Cache) gin.HandlerFunc {
 	return func(c *gin.Context) {
+
 		icon := identicon.New7x7(Key)
 		name := c.Param("name")
-
-		l.Info("Encoding identicon", "name", name)
-
 		data := []byte(name)
-		pngdata := icon.Render(data)
-		encoding := base64.StdEncoding.EncodeToString(pngdata)
-		c.Writer.Write([]byte("data:image/png;base64," + encoding))
+		if len(data) > 255 {
+			c.String(400, "Name too long")
+			return
+		}
+
+		val, err := cache.Get(data)
+		if err != nil || len(val) == 0 {
+			l.Info("Encoding identicon", "name", name)
+			pngdata := icon.Render(data)
+			encoding := base64.StdEncoding.EncodeToString(pngdata)
+			val = []byte("data:image/png;base64," + encoding)
+
+			l.Info("Loading cache")
+			if err = cache.Set(data, val, 0); err != nil {
+				l.Info("Failed to load cache")
+			}
+		}
+
+		c.Writer.Write(val)
 		c.Header("Content-Type", "text/plain")
 	}
 }
